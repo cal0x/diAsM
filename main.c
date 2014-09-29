6c3e19c4abe38577877c28ec3e532d7aa7812a48
cal0x <cal0xmail@gmail.com>
2014-09-29 21:31:34 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
 #include <udis86.h>
 
-int main(int argc, char* argv[])
+void main(int argc, char* argv[])
 {
-	FILE *file = fopen(argv[1], "rw++");
 
-	ud_t ud_obj;
-	
-	ud_init(&ud_obj);
-	ud_set_input_file(&ud_obj, file);
-	ud_set_mode(&ud_obj, 32);
-	ud_set_syntax(&ud_obj, UD_SYN_INTEL);
-
-	while (ud_disassemble(&ud_obj))
+	if ( argc == 2)
 	{
-		printf("\t%s\n", ud_insn_asm(&ud_obj));
+		FILE *file = fopen(argv[1], "rw++");
+
+		ud_t ud_obj;
+		
+		ud_init(&ud_obj);
+		ud_set_input_file(&ud_obj, file);
+		ud_set_mode(&ud_obj, 32);
+		ud_set_syntax(&ud_obj, UD_SYN_INTEL);
+
+		while (ud_disassemble(&ud_obj))
+		{
+			printf("\t%s\n", ud_insn_asm(&ud_obj));
+		}
+
+	    fclose(file);
 	}
-	
-	fclose(file);
-	return 0;
 }
