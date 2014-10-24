389ddf6393aeb2bd07c450996a6e351e5de8ad61
cal0x <cal0xmail@gmail.com>
2014-10-25 00:25:13 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,25 +1,78 @@
-#include <stdio.h>
 #include <udis86.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
 
-void main(int argc, char* argv[])
+
+void diasassemble( FILE *file )
+{
+    ud_t ud_obj;
+
+    ud_init(&ud_obj);
+    ud_set_input_file(&ud_obj, file);
+    ud_set_mode(&ud_obj, 32);
+    ud_set_syntax(&ud_obj, UD_SYN_INTEL);
+
+    while (ud_disassemble(&ud_obj))
+    {
+        printf("\t%s\n", ud_insn_asm(&ud_obj));
+    }
+}
+
+void hexdump( FILE *file )
 {
 
-	if ( argc == 2)
-	{
-		FILE *file = fopen(argv[1], "rw++");
+}
 
-		ud_t ud_obj;
-		
-		ud_init(&ud_obj);
-		ud_set_input_file(&ud_obj, file);
-		ud_set_mode(&ud_obj, 32);
-		ud_set_syntax(&ud_obj, UD_SYN_INTEL);
+void showhelp()
+{
+    printf(
+           "\n\tFunctions:\n%s%s%s%s",
+           "\t\tHelp: -h or --help\n",
+           "\t\tDisassemble: -d or --disass - file\n",
+           "\t\tHexdump: -hd or --hexdump - file\n",
+            "\t\Bytes to ASM: -ba or --bytestoasm\n",
+           "\n"
+           );
+}
 
-		while (ud_disassemble(&ud_obj))
-		{
-			printf("\t%s\n", ud_insn_asm(&ud_obj));
-		}
+int main(int argc, char* argv[])
+{
+    char* argument = argv[1];
+    char* filename;
 
-	    fclose(file);
-	}
+    if ( argc > 1 && argc <= 3 )
+    {
+        filename = argv[2];
+        FILE *file = fopen(filename, "rw++");
+
+        if ( strcmp("-h", argument) == 0 || strcmp("--help", argument) == 0 )
+        {
+            showhelp();
+        }
+        else if ( strcmp("-d", argument) == 0 || strcmp("--disass", argument) == 0 )
+        {
+            diasassemble( file );
+        }
+        else if ( strcmp("-hd", argument) == 0 || strcmp("--hexdump", argument) == 0 )
+        {
+            hexdump( file );
+        }
+        else if ( strcmp("-ba", argument) == 0 || strcmp("--bytestoasm", argument) == 0 )
+        {
+            diasassemble( stdin );
+        }
+        else
+        {
+            showhelp();
+        }
+    }
+    else
+    {
+        showhelp();
+    }
+
+    return 0;
 }
